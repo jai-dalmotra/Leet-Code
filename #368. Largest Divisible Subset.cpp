3612012e069712b5dbd07d27723af3590472e8da
Jai Dalmotra <168071500+jai-dalmotra@users.noreply.github.com>
2025-04-08 01:56:40 +0530

    Update #368. Largest Divisible Subset.cpp

--- #368. Largest Divisible Subset.cpp
+++ #368. Largest Divisible Subset.cpp
@@ -1,1 +1,37 @@
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return {};
 
+        sort(nums.begin(), nums.end());
+        
+        vector<int> dp(n, 1), prev(n, -1);
+        int maxSize = 1, maxIndex = 0;
+
+        // Fill DP and previous index arrays
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+
+            if (dp[i] > maxSize) {
+                maxSize = dp[i];
+                maxIndex = i;
+            }
+        }
+
+        // Reconstruct the subset
+        vector<int> result;
+        for (int i = maxIndex; i >= 0; i = prev[i]) {
+            result.push_back(nums[i]);
+            if (prev[i] == -1) break;
+        }
+
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
